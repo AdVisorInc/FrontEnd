66bcaaf07660b9eb6d82f0494e7cdacaff274c0d
gelbling <gelbling10@gmail.com>
2024-02-16 02:05:21 -0500

    dashboard + sidebar + headerNav

--- advisorfrontend/src/App.tsx
+++ advisorfrontend/src/App.tsx
@@ -2,48 +2,30 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./Components/Pages/LoginPage";
 import RegisterPage from "./Components/Pages/RegisterPage";
+import DashboardPage from "./Components/Pages/DashboardPage";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#007BFF", // Example primary color
-    },
-    secondary: {
-      main: "#F50057", // Example secondary color
-    },
-  },
-  components: {
-    MuiTextField: {
-      defaultProps: {
-        variant: "outlined", // Use outlined variant for text fields by default
-        margin: "normal",
-      },
-    },
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          margin: "8px",
-        },
-      },
-    },
-  },
-});
+import ThemeProviderWrapper from "./theme/ThemeProvider";
+import { HelmetProvider } from "react-helmet-async";
+import SidebarLayout from "./SidebarLayout";
 
 const App: React.FC = () => {
   return (
-    <GoogleOAuthProvider clientId="22202495353-q4duq21a6saollon2psplpgs5crl5oc4.apps.googleusercontent.com">
-      <ThemeProvider theme={theme}>
-        <Router>
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/" element={<LoginPage />} />
-          </Routes>
-        </Router>
-      </ThemeProvider>
-      ,
-    </GoogleOAuthProvider>
+    <HelmetProvider>
+      <ThemeProviderWrapper>
+        <GoogleOAuthProvider clientId="22202495353-q4duq21a6saollon2psplpgs5crl5oc4.apps.googleusercontent.com">
+          <Router>
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/dashboard" element={<SidebarLayout />}>
+                <Route path=":overview" element={<DashboardPage />} />
+              </Route>
+              <Route path="/" element={<LoginPage />} />
+            </Routes>
+          </Router>
+        </GoogleOAuthProvider>
+      </ThemeProviderWrapper>
+    </HelmetProvider>
   );
 };
 
--- advisorfrontend/src/index.tsx
+++ advisorfrontend/src/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { SidebarProvider } from "./contexts/SidebarContext";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <SidebarProvider>
+      <App />
+    </SidebarProvider>
   </React.StrictMode>
 );
 
