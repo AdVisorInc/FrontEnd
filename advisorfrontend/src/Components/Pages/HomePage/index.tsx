f88678c8041a2229b1040b440b35fbd2bcd650f2
Zach Farley <zfarley2123@gmail.com>
2024-02-29 17:13:57 -0500

    Started making home page responsive

--- advisorfrontend/src/Components/Pages/HomePage/index.tsx
+++ advisorfrontend/src/Components/Pages/HomePage/index.tsx
@@ -2,36 +2,46 @@ import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
-import { Container, Box, Typography } from "@mui/material";
+import {
+  Container,
+  Box,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 
 import Avatar from "@mui/material/Avatar";
 import { useNavigate } from "react-router-dom";
+import { Dict } from "styled-components/dist/types";
 
 const HomePage: React.FC = () => {
-  const UserAvatars = () => {
-    const platformCircles = [
-      {
-        alt: "TikTok",
-        src: `tiktok_logo.png`,
-        positions: { top: "8em", left: "1em" },
-      },
-      {
-        alt: "Google",
-        src: "google_logo.png",
-        positions: { bottom: "2em", left: "8em" },
-      },
-      {
-        alt: "Meta",
-        src: "meta_logo.png",
-        positions: { top: "6em", right: "0em" },
-      },
-      {
-        alt: "X",
-        src: "x_logo.png",
-        positions: { bottom: "6em", right: "8em" },
-      },
-    ];
+  const theme = useTheme();
+  const renderDesktopVersion = useMediaQuery(theme.breakpoints.up("md"));
 
+  const platformCircles = [
+    {
+      alt: "TikTok",
+      src: `tiktok_logo.png`,
+      positions: { top: "8em", left: "1em" },
+    },
+    {
+      alt: "Google",
+      src: "google_logo.png",
+      positions: { bottom: "2em", left: "8em" },
+    },
+    {
+      alt: "Meta",
+      src: "meta_logo.png",
+      positions: { top: "6em", right: "0em" },
+    },
+    {
+      alt: "X",
+      src: "x_logo.png",
+      positions: { bottom: "6em", right: "8em" },
+    },
+  ];
+
+  const UserAvatarsDesktop = () => {
     return (
       <>
         {platformCircles.map((element) => {
@@ -54,10 +64,148 @@ const HomePage: React.FC = () => {
     );
   };
 
+  interface IHateTypeScript {
+    alt: string;
+    src: string;
+    positions: any;
+  }
+
+  const UserAvatarsMobile = () => {
+    const mobileAvatarCircle = (element: IHateTypeScript) => {
+      return (
+        <Avatar
+          alt={element.alt}
+          sx={{
+            width: 48,
+            height: 48,
+            boxShadow: "0 2px 6px rgba(255, 255, 255, .15)",
+          }}
+          src={`${process.env.PUBLIC_URL}/social_icons/${element.src}`}
+        />
+      );
+    };
+
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: "50%",
+          gap: 2,
+          paddingTop: 4,
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          {mobileAvatarCircle(platformCircles[0])}
+          {mobileAvatarCircle(platformCircles[3])}
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "75%",
+          }}
+        >
+          {mobileAvatarCircle(platformCircles[2])}
+          {mobileAvatarCircle(platformCircles[1])}
+        </Box>
+      </Box>
+    );
+  };
+
+  const HeroTitle = () => {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          gap: {
+            xs: 0,
+            sm: 0,
+            md: 2,
+            lg: 2,
+            xl: 2,
+          },
+          my: 2,
+          textAlign: "center",
+          flexDirection: {
+            xs: "column",
+            sm: "column",
+            md: "row",
+            lg: "row",
+            xl: "row",
+          },
+        }}
+      >
+        <Typography
+          sx={{
+            fontWeight: "bold",
+            color: "white",
+            letterSpacing: 2,
+            fontSize: {
+              xs: 32,
+              sm: 52,
+              md: 52,
+              lg: 52,
+              xl: 52,
+            },
+          }}
+        >
+          Excel Without
+        </Typography>
+        <Typography
+          sx={{
+            backgroundImage: "linear-gradient(to right, #DE0D92, #4D6CFA)",
+            color: "transparent",
+            backgroundClip: "text",
+            // Cross-browser compatibility
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+            fontWeight: "bold",
+            textAlign: "center",
+            letterSpacing: 2,
+            fontSize: {
+              xs: 32,
+              sm: 52,
+              md: 52,
+              lg: 52,
+              xl: 52,
+            },
+          }}
+        >
+          Excel
+        </Typography>
+      </Box>
+    );
+  };
+
   const HeroSection = () => {
     return (
-      <Container sx={{ paddingTop: 12, position: "relative" }}>
-        <UserAvatars />
+      <Container
+        sx={{
+          paddingTop: {
+            xs: 2,
+            sm: 4,
+            md: 12,
+            lg: 12,
+            xl: 12,
+          },
+          position: "relative",
+        }}
+      >
+        {renderDesktopVersion ? <UserAvatarsDesktop /> : <div />}
         <Box
           sx={{
             display: "flex",
@@ -66,38 +214,21 @@ const HomePage: React.FC = () => {
             alignItems: "center",
           }}
         >
-          <Box sx={{ display: "flex", gap: 2, my: 2, textAlign: "center" }}>
-            <Typography
-              variant="h1"
-              sx={{ fontWeight: "bold", color: "white", letterSpacing: 2 }}
-            >
-              Excel Without
-            </Typography>
-            <Typography
-              variant="h1"
-              sx={{
-                backgroundImage: "linear-gradient(to right, #DE0D92, #4D6CFA)",
-                color: "transparent",
-                backgroundClip: "text",
-                // Cross-browser compatibility
-                WebkitBackgroundClip: "text",
-                WebkitTextFillColor: "transparent",
-                fontWeight: "bold",
-                textAlign: "center",
-                letterSpacing: 2,
-              }}
-            >
-              Excel
-            </Typography>
-          </Box>
+          <HeroTitle />
           <Typography
-            variant="h6"
             fontWeight="regular"
             gutterBottom
             color={"white"}
             sx={{
+              variant: "h6",
               textAlign: "center",
-              width: "48%",
+              width: {
+                xs: "90%",
+                sm: "75%",
+                md: "48%",
+                lg: "48%",
+                xl: "48%",
+              },
               opacity: 0.5,
               lineHeight: 1.25,
             }}
@@ -105,6 +236,7 @@ const HomePage: React.FC = () => {
             Integrate TikTok, Google, and more to compare your advertising
             campaigns across platforms and launch new ones.
           </Typography>
+          {!renderDesktopVersion ? <UserAvatarsMobile /> : <div />}
           <HeroButtons />
         </Box>
       </Container>
@@ -183,7 +315,11 @@ const HomePage: React.FC = () => {
     const navigate = useNavigate();
 
     return (
-      <AppBar position="static" sx={{ background: "#0d1b2a" }} elevation={0}>
+      <AppBar
+        position="static"
+        sx={{ background: "#0d1b2a", paddingTop: 4 }}
+        elevation={0}
+      >
         <Container>
           <Toolbar
             disableGutters
